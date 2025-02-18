c4ddbd52c580a06c2721cd3e4d86fc39d6421be0
FujiBookPro <maczone06@gmail.com>
2025-02-17 22:12:10 -0500

    Update resolution and bird count

--- app/src/main/java/boids/App.java
+++ app/src/main/java/boids/App.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 import com.raylib.Colors;
 
 public class App {
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 450;
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
     public static void main(String[] args) {
         Raylib.InitWindow(WIDTH, HEIGHT, "Demo");
-        Raylib.SetTargetFPS(60);
+        Raylib.SetTargetFPS(30);
 
         var birds = new ArrayList<Bird>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 50; i++) {
             birds.add(Bird.random());
         }
 
